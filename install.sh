28ed0210dcad23c1d6086bc5bbce1303e6c84c34
Jannick Bath <jannickbath@gmail.com>
2023-11-13 10:33:50 +0100

    *improvements

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root."
   exit
 fi
 
-if ! command -v docker &> /dev/null
-then
+if ! command -v docker > /dev/null 2>&1; then
     echo "Docker is not installed. Installing..."
     apt install docker
+else
+    echo "Docker is already installed."
 fi
 
-if ! command -v docker-compose &> /dev/null
-then
+if ! command -v docker-compose > /dev/null 2>&1; then
     echo "docker-compose is not installed. Installing..."
     apt install docker-compose
+else
+    echo "docker-compose is already installed."
 fi
 
-if ! command -v mkcert &> /dev/null
-then
+if ! command -v mkcert > /dev/null 2>&1; then
     echo "mkcert is not installed. Installing..."
 
     apt install libnss3-tools
@@ -28,6 +29,8 @@ then
 
     mkcert -install
     mkcert -key-file ./certs/key.pem -cert-file ./certs/cert.pem $(cat ./certs/domains.txt)
+else
+    echo "mkcert is already installed."
 fi
 
 if systemctl is-active --quiet apache2 ; then
@@ -38,7 +41,7 @@ else
 fi
 
 if [ ! -d "~/db_data" ]; then
-    if mkdir -p ~/db_data ; then
+    if mkdir ~/db_data ; then
         echo "Created directory ~/db_data"
     else
         echo "Failed to create ~/db_data. Skipping..."
@@ -47,22 +50,34 @@ else
     echo "Directory ~/db_data already exists. Skipping..."
 fi
 
-if docker network create traefik_net ; then
-    echo "Created docker network 'traefik_net'"
+if docker network ls | grep traefik_net > /dev/null 2>&1; then
+    echo "Docker network traefik_net already exists. Skipping..."
 else
-    echo "Failed to create docker network 'traefik_net'. Skipping..."
+    if docker network create traefik_net > /dev/null 2>&1; then
+        echo "Created docker network 'traefik_net'"
+    else
+        echo "Failed to create docker network 'traefik_net'. Skipping..."
+    fi
 fi
 
-if docker network create db_network ; then
-    echo "Created docker network 'db_network'"
+if docker network ls | grep db_network > /dev/null 2>&1; then
+    echo "Docker network db_network already exists. Skipping..."
 else
-    echo "Failed to create docker network 'db_network'. Skipping..."
+    if docker network create db_network > /dev/null 2>&1; then
+        echo "Created docker network 'db_network'"
+    else
+        echo "Failed to create docker network 'db_network'. Skipping..."
+    fi
 fi
 
-if docker volume create portainer_data ; then
-    echo "Created docker volume 'portainer_data'"
+if docker volume ls | grep portainer_data > /dev/null 2>&1; then
+    echo "portainer_data volume already exists. Skipping..."
 else
-    echo "Failed to create docker volume 'portainer_data'. Skipping..."
+    if docker volume create portainer_data > /dev/null 2>&1; then
+        echo "Created docker volume 'portainer_data'"
+    else
+        echo "Failed to create docker volume 'portainer_data'. Skipping..."
+    fi
 fi
 
 docker-compose up -d
@@ -105,8 +120,10 @@ fi
 MysqlContainerId=$(docker ps -aqf "name=mysql" | head -n 1)
 MysqlIp=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $MysqlContainerId)
 
-if mysql --host=$MysqlIp --user=root -popwer384 ; then
-    echo "MySQL Connection successfull."
+mysql --host=$MysqlIp --user=root -popwer384 > /dev/null 2>&1 < /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "Connection successful!"
 else
-    echo "Failed to connect to MySQL"
+  echo "Connection failed."
 fi
